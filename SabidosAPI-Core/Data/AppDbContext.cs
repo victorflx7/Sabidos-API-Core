0835f7fcee8a55aba06063a90cfb832115f2a484
WillianSCorrea <willianwcorrea@gmail.com>
2025-09-10 11:56:00 -0300

    Configuração do Contexto de Banco - AppDbContext #11 Implementa AppDbContext com DbSets e índice único
    Adicionadas novas importações e implementada a classe `AppDbContext`, que herda de `DbContext`. Definidas propriedades `DbSet` para as entidades `User`, `Resumo`, `Evento`, `Pomodoro` e `Flashcard`. Configurado índice único para o campo `FirebaseUid` na entidade `User` no método `OnModelCreating`.

---
+++ SabidosAPI-Core/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+using FirebaseJwtApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SabidosAPI_Core.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Resumo> Resumos => Set<Post>();
+    public DbSet<Evento> Eventos => Set<Post>();
+    public DbSet<Pomodoro> Pomodoros => Set<Post>();
+    public DbSet<Flashcard> Flashcards => Set<Post>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasIndex(u => u.FirebaseUid).IsUnique();
+        base.OnModelCreating(modelBuilder);
+    }
+}
+  
