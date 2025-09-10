cb2a2d2d770e7ed008d2e8e31f66b2a38d2ee892
WillianSCorrea <willianwcorrea@gmail.com>
2025-09-10 01:48:16 -0300

    Estrutura inicial da Api (Arquitetura de Pastas e Solução do Projeto) #1 Adiciona configuração inicial do projeto ASP.NET Core
    - Atualiza `.gitignore` e `.dockerignore` para ignorar arquivos temporários e de configuração.
    - Cria o controlador `UserController` para a API.
    - Modifica o `Dockerfile` para definir fases de construção e publicação.
    - Configura o `Program.cs` para adicionar serviços e Swagger.
    - Adiciona `launchSettings.json` para perfis de execução, incluindo Docker.
    - Atualiza `SabidosAPI-Core.csproj` para .NET 8.0 e referências de pacotes.
    - Cria `SabidosAPI-Core.http` com exemplo de requisição HTTP.
    - Atualiza `SabidosAPI-Core.sln` para incluir o novo projeto.
    - Adiciona `appsettings.Development.json` e `appsettings.json` para configuração de log.

---
+++ SabidosAPI-Core/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SabidosAPI_Core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+       
+
+    }
+}
