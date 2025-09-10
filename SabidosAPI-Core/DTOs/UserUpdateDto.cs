242e5735dfbe0b1c7ef4d38e88fc222055071e1b
WillianSCorrea <willianwcorrea@gmail.com>
2025-09-10 15:40:37 -0300

    Criação do DTO UserUpdateDto #13
    Adiciona classe UserUpdateDto com validação de nome
    
    Nova classe `UserUpdateDto` criada para gerenciar atualizações de usuários. Inclui uma propriedade `Name` com limite de 160 caracteres. Comentários indicam planos futuros para expandir a funcionalidade, permitindo o envio de dados adicionais, como imagens de perfil, para o banco de dados SQL Server.

---
+++ SabidosAPI-Core/DTOs/UserUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SabidosAPI_Core.DTOs
+{
+    public class UserUpdateDto
+    {
+        /// Essa função se está sendo criada principalmente para futuramente , Quando existia estiver Mais estruturado e robusto Não só atualizar
+        /// o nome de usuário Mas também inserir nos dados do banco de dados SQL server, O link de referência talvez de alguma imagem ou arquivo 
+        /// de perfil do usuário vindo do front end e do subabase 
+        [StringLength(160)]
+        public string? Name { get; set; }
+    }
+}
