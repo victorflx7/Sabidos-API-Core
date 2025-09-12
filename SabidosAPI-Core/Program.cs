ffab76181330fddeec2216e11745b2aa57671c7c
WillianSCorrea <willianwcorrea@gmail.com>
2025-09-12 14:58:30 -0300

    Configuração da Autenticação JWT no Program.cs #16
    Configuração do JWT Bearer (Firebase) , De configurações de build services deo AutoMapper , UserService e EF Core + SQL Server

--- SabidosAPI-Core/Program.cs
+++ SabidosAPI-Core/Program.cs
@@ -1,11 +1,48 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SabidosAPI_Core.Data;
+using SabidosAPI_Core.Mappings;
+using SabidosAPI_Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+// EF Core + SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// AutoMapper
+builder.Services.AddAutoMapper(typeof(UserProfile));
+
+// Services (por favor colocar todos neste grupo)
+builder.Services.AddScoped<UserService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// JWT Bearer (Firebase)
+var firebaseProjectId = builder.Configuration["Firebase:ProjectId"];
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = $"https://securetoken.google.com/{firebaseProjectId}";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = $"https://securetoken.google.com/{firebaseProjectId}",
+            ValidateAudience = true,
+            ValidAudience = firebaseProjectId,
+            ValidateLifetime = true
+        };
+    });
+
 
 var app = builder.Build();
 
