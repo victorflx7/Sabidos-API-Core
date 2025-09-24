0b01bd74dc5bf7662084eaa4bca6363a8c16525c
Victor <victorsantosflx@gmail.com>
2025-09-24 18:22:34 -0300

    DTO para criação e atualização de flashcards (FlashcardCreateUpdateDto)

---
+++ SabidosAPI-Core/DTOs/FlashcardCreateUpdateDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SabidosAPI_Core.DTOs
+{
+    public class FlashcardCreateUpdateDto
+    {
+        [Required]
+        [MaxLength(160)]
+        public string Titulo { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(8000)]
+        public string Frente { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(8000)]
+        public string Verso { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(160)]
+        public string AuthorUid { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(160)]
+        public string AuthorName { get; set; } = string.Empty;
+    }
+}
