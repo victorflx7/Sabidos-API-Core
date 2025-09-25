c41a9fd5e924446fe490bbaeebdc76a4bf63a097
WillianSCorrea <willianwcorrea@gmail.com>
2025-09-25 10:05:30 -0300

    Criar Classe Flashcard no Models #75  Criar entidade para regra de Negocia referentes a fução e modelo de Flashcard , como Base tabela e modelo de respostas e Requicisões

---
+++ SabidosAPI-Core/Models/Flashcard.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SabidosAPI_Core.Models
+{
+    public class Flashcard
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(160)]
+        public string Titulo { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(8000)]
+        public string Frente { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(8000)]
+        public string Verso { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(160)]
+        public string AuthorUid { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(160)]
+        public string AuthorName { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public DateTime UpdatedAt { get; set; }
+
+        public User User { get; set; } = null!;
+    }
+}
