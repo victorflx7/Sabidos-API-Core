31facd2d3a97351bc5e9ee18169ab3754ddfefef
Vitaminazz <vitamina.twitu@gmail.com>
2025-09-25 21:02:46 -0300

    Criação do create DTO do pomodoro

---
+++ SabidosAPI-Core/DTOs/PomoCreateDto.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SabidosAPI_Core.Dtos
+{
+    public class PomoCreateDto
+    {
+        [Required]
+        public int Ciclos { get; set; }
+
+        [Required]
+        public int Duration { get; set; }
+
+        [Required]
+        public int TempoTrabalho { get; set; }
+
+        [Required]
+        public int TempoDescanso { get; set; }
+
+        [Required]
+        public int Userid { get; set; }
+
+        [Required]
+        [MaxLength(160)]
+        public string AuthorUid { get; set; } = string.Empty;
+    }
+}
